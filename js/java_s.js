1ed2ad4d712f40f3bdb4a1d4d74a77009a44ac29
Mr.Igor <iv_mirsoft@ukr.net>
2025-03-20 09:34:05 +0200

    mobile-menu

--- js/java_s.js
+++ js/java_s.js
@@ -1,20 +1,22 @@
 
-var btnOpen = document.getElementById("open_form");
-var btnClose = document.getElementById("close_form");
-var myModalForm = document.getElementById("modal_form");
-
+var myBurgerIcon = document.getElementById("burger-icon");
 var btnOpenMenu = document.getElementById("open_menu");
-var btnCloseMenu = document.getElementById("close_menu");
+
 var myModalMenu = document.getElementById("modal_menu");
 
 function toggleClass(objForm) {
+	console.log("pressed");
+
 	return function () {
 		objForm.classList.toggle("is-open");
+
+		if (objForm.classList.contains("is-open")) {
+			myBurgerIcon.setAttribute("href", "./images/icons.svg#btn-close");
+		} else {
+			myBurgerIcon.setAttribute("href", "./images/icons.svg#burger");
+		}
+
 	};
 }
 
-btnOpen.addEventListener("click", toggleClass(myModalForm));
-btnClose.addEventListener("click", toggleClass(myModalForm));
-
 btnOpenMenu.addEventListener("click", toggleClass(myModalMenu));
-btnCloseMenu.addEventListener("click", toggleClass(myModalMenu));
